193fce456f1cd46a15d5659e2681b35e20407349
bhupeshkhedikar <bhupesh.khedikar@extwebtech.in>
2023-03-13 18:29:06 +0530

    latest blog and down part added

---
+++ src/assets/data/coursesDataBox.tsx
@@ -0,0 +1,43 @@
+export interface IProps {
+  id: number;
+  imgUrl: string;
+  title: string;
+  subTitle: string;
+    date: string;
+}
+
+export const courseImages = [
+  { id: 1, imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-09-1.svg" },
+  { id: 2, imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-10.svg" },
+  { id: 3, imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-16.svg" },
+  { id: 4, imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-12.svg" },
+  { id: 5, imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-13.svg" },
+  { id: 6, imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-14.svg" },
+  { id: 7, imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-15.svg" },
+  { id: 8, imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-16-1.svg" },
+  { id: 9, imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-17.svg" },
+  { id: 10,imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/icon-18.svg" },
+];
+
+export const latestBlogs=[
+    { id: 1, title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg" },
+    { id: 2, title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg" },
+    { id: 3, title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg" },
+    { id: 4, title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg" },
+    // { id: 5,title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    // imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg" },
+    // { id: 6,title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    // imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg" },
+    // { id: 7,title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    // imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg"},
+    // { id: 8, title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    // imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg" },
+    // { id: 9, title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    // imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg" },
+    // { id: 10,title: "Expand Your Career Opportunities With Python", subTitle: "Courses", date: "January 18, 2023",
+    // imgUrl: "https://dreamslms-wp.dreamguystech.com/wp-content/uploads/2023/01/blog-02-600x400.jpg"},
+  ];
